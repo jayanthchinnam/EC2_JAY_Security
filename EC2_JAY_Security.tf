58011afeb63912818adf069922cad9f1d4683820
jayanthtechm <jayanthtechm@gmail.com>
2022-06-22 08:07:21 +0530

    changes code

--- EC2_JAY_Security.tf
+++ EC2_JAY_Security.tf
@@ -4,9 +4,23 @@ provider "aws" {
 
 
 
-resource "aws_security_group" "allow_jay" {
-  name        = "allow_jay"
-  description = "Allow jay traffic"
+#Ec2instance
+
+module "ec2_instance" {
+  ami                    = "ami-08df646e18b182346"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group_ids.allow_jay.id]
+
+  tags = {
+    name  = "allow_sample"
+  }
+}
+
+#security group creation
+
+resource "aws_security_group" "allow_sample" {
+  name        = "allow_sample"
+  description = "allow_sample traffic"
   
 
   ingress  =  [
@@ -17,10 +31,14 @@ resource "aws_security_group" "allow_jay" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = []
+    Prefix_list_id   = []
+    security_groups  = []
+    self             = false
   }
 ]
   egress  =  [
   {
+    description      = "egress"  
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -32,3 +50,4 @@ resource "aws_security_group" "allow_jay" {
     Name = "allow_jay"
   }
 }
+
